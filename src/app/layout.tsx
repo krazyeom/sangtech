418d858f6701c28f91a214f28625b14b4e2d34bd
Sungwook Yeom <krazyeom@gmail.com>
2026-06-11 23:39:01 +0900

    style: remove notifications tab from navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
               <a href="/market-calculator">시세 계산기</a>
               <a href="/calculator">계산기</a>
               <a href="/automation">자동화</a>
-              <a href="/notifications">알림</a>
               <a href="/changelog">업데이트 노트</a>
             </div>
           </div>
